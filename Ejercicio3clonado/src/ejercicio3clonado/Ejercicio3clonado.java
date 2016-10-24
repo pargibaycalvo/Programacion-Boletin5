29f87f1f16a9d2d3743e04a68f364c5423bd5c22
Ped90 <Ped90@Ped90>
2016-10-24 09:12:32 +0200

    ejercicio3cloando

---
+++ Ejercicio3clonado/src/ejercicio3clonado/Ejercicio3clonado.java
@@ -0,0 +1,13 @@
+
+package ejercicio3clonado;
+
+public class Ejercicio3clonado {
+
+    public static void main(String[] args) {
+        // 
+        float holaaaaaaaa;
+        
+        
+    }
+    
+}
